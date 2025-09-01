2ff478a8fbc3121a0a94c61620d6f8e0707b1b6b
juschneider <JuSchneider@atb-potsdam.de>
2025-09-01 18:50:07 +0200

    Created glossary, deleted old README, commented code

--- program.js
+++ program.js
@@ -1,36 +1,43 @@
+// Load data from XML file and start main process
 function loadDoc(id) {
   var xmlRequest = new XMLHttpRequest();
   xmlRequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        loadQuestion(this,id);
+        loadQuestion(this,id); //Call to main fuction
     }
   };
   xmlRequest.open("GET", "./data.xml", true);
   xmlRequest.send();
 }
 
+//Update page with the data for the provided question index
 function loadQuestion(xml,id)
 {
-    var xmlDoc = xml.responseXML;
-    var entries = xmlDoc.getElementsByTagName("ENTRY");
-
+    var xmlDoc = xml.responseXML; //Get XML document
+    var entries = xmlDoc.getElementsByTagName("ENTRY"); //List of all "ENTRY" entries in the XML document
 
     RemoveButtons();
     UpdateProgressBar(entries, id);
 
     var question_content;
     var title_content;
-    for (var i = 0; i <entries.length; i++) { 
+    
+    //Find entry with the same ID as the provided index
+    for (var i = 0; i <entries.length; i++)
+    {
+        //Case: Entry found 
         if(entries[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue == id)
         {
             question_content = entries[i].getElementsByTagName("QUESTION_TEXT")[0].childNodes[0].nodeValue;
             title_content = entries[i].getElementsByTagName("CATEGORY")[0].childNodes[0].nodeValue;
+            
             AddButtons(entries[i]);
-
-            //Change Border Colour
             SetQuestionBorderColour(entries[i]);
             break;
-        }else{
+        }
+        //Case: No entry with the given ID
+        else
+        {
             question_content = "Eintrag " + id + " nicht gefunden";
             title_content = "Titel zum Eintrag " + id + " nicht gefunden";
         }
@@ -42,6 +49,7 @@ function loadQuestion(xml,id)
     
 }
 
+//Removes all elements with the ID "button"
 function RemoveButtons()
 {
     while(document.getElementById("button"))
@@ -51,6 +59,7 @@ function RemoveButtons()
     }
 }
 
+//Removes all elements with the ID "circle"
 function RemoveHierachy()
 {
     while(document.getElementById("circle"))
@@ -60,10 +69,12 @@ function RemoveHierachy()
     }
 }
 
+//Adds a circle note for a provided entry
 function AddHierachy(entry, id)
 {
+  //Return if the entry is not a question
   if(entry.getElementsByTagName("TYPE")[0].childNodes[0].nodeValue != 'Frage'){return;}
-  if(entry.getElementsByTagName("ID")[0].childNodes[0].nodeValue == 0){return;}
+  //Return if the entry exceeds the index
   if(parseInt(entry.getElementsByTagName("ID")[0].childNodes[0].nodeValue) > id){return;}
 
   let page_link = entry.getElementsByTagName("ID")[0].childNodes[0].nodeValue;
@@ -82,12 +93,14 @@ function AddHierachy(entry, id)
   document.getElementById("container hierarchy").appendChild(newDiv);
 }
 
+//Adds every button entry of a provided question entry
 function AddButtons(entry)
 {
-    var buttons_content = entry.getElementsByTagName("BUTTON_TEXT");
-    var buttons_links = entry.getElementsByTagName("BUTTON_LINK");
-    const container = document.getElementById("container answer");
+    var buttons_content = entry.getElementsByTagName("BUTTON_TEXT"); //List of all the texts for all the buttons
+    var buttons_links = entry.getElementsByTagName("BUTTON_LINK"); //List of all the links for all the buttons
+    const container = document.getElementById("container answer"); //Div for positioning buttons of an entry
 
+    //creates a new button element for every BUTTON_TEXT entry
     for(var i = 0; i < buttons_content.length; i++)
     {
       let newButton = document.createElement('button');
@@ -105,36 +118,41 @@ function AddButtons(entry)
       container.appendChild(newButton);
     }
 
-    // document.getElementById("demo").innerText = "Fun" + buttons_content.length;
     return;
 }
 
+//Update progress bar to the provided index
 function UpdateProgressBar(entries, id)
 {
   var progressBar = document.getElementById("progress bar");
 
     let questionCounter = 0;
 
-    //Festlegung des Fortschritts nach einer Exitfrage auf 0
+    //Setting the progress after an exit question to 0
     if(id == 0)
     {
       progressBar.value = 0;
     }
 
-    //Dynamische Bestimmung des Maximalwertes und des aktuellen Wertes der Fortschrittsleiste
+    //Set current and max value
     for (var i = 0; i <entries.length; i++) 
     {
+      //Dynamic determination of the maximum value by counting all "Frage" (questions) entries
       if(entries[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue == "Frage")
       {
         questionCounter++;
       }
 
+      //Setting progress value
       if(entries[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue == id)
       {
+        //Case: question ("Frage")
         if(entries[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue == "Frage")
         {
-          progressBar.value = id-1;          
-        }else if(entries[i].getElementsByTagName("BUTTON_LINK").length == 0)
+          progressBar.value = id-1; //The IDs of question start with 1. Therefore, they need to be reduced by 1 as the current question has not been completed and therefore does not count towards the achieved progress.     
+        }
+        //Case: End of the tree (signified by having no further links to other entries)
+        else if(entries[i].getElementsByTagName("BUTTON_LINK").length == 0)
         {
           progressBar.value = parseInt(progressBar.value) + 1;
         }
@@ -146,34 +164,18 @@ function UpdateProgressBar(entries, id)
     progressBar.max = questionCounter;
 }
 
+// Set border colour of the div "container question" based on the TYPE of the entry
 function SetQuestionBorderColour(entry)
 {
   if(entry.getElementsByTagName("TYPE")[0].childNodes[0].nodeValue =="Antwort")
   {
-    document.getElementById("container question").className = "container question notice";
+    document.getElementById("container question").className = "container question notice"; // Additional remarks: hsl(84, 55%, 55%) - green
   }else if(entry.getElementsByTagName("TYPE")[0].childNodes[0].nodeValue =="Exitfrage")
   {
-    document.getElementById("container question").className = "container question warning";
+    document.getElementById("container question").className = "container question warning"; // Exitquestion: hsl(6, 85%, 66%) - red
   }
   else
   {
-    document.getElementById("container question").className = "container question";
+    document.getElementById("container question").className = "container question"; //default case: hsl(0, 0%, 100%) - white
   }
 }
-
-/*
-function t(xml,id) {
-  var xmlDoc = xml.responseXML;
-  var x = xmlDoc.getElementsByTagName("CD");
-  var table="<tr><th>Artist</th><th>Title</th></tr>";
-  for (var i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue + id +
-    "</td></tr>";
-  }
-  document.getElementById("demo").innerHTML = table;
-} */
