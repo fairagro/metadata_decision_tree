1ed3fc0e19362f679d68c28c9bf79fb1dc4c160b
EinzJustus <Justus.H.Schneider@gmx.de>
2025-09-16 18:04:15 +0200

    Added autotranlation using gtranslate, navigation to glossary and better code documentetion, slight clean up, fixed disapearing navigation after zooming and interacting with the mobile version, deaktivated translation using the english data file

--- js/language_selector.js
+++ js/language_selector.js
@@ -1,17 +1,19 @@
-const germanButton = document.getElementById("german");
-const englishButton = document.getElementById("english");
-const languageButtons = document.querySelectorAll('a[id*="language"]');
+const languageButtons = document.querySelectorAll('a[id*="language"]'); //Array of all language buttons
 let currentLanguage = "german";
 
+//Sets the current language and updates the UI
 function SetLanguage(language) {
     currentLanguage = language;
     ResetSelectedButton();
     document.getElementById(`language ${language}`).className = "language-selected";
     //document.getElementById("demo").innerText = currentLanguage;
-    currentDataPath = `./data_${language}.xml`;
+
+    //Set current data path to the selected language
+    currentDataPath = `./data_${language}.xml`; 
     LoadDoc(prevID.at(-1))
 }
 
+//Sets all language buttons to their unselected design
 function ResetSelectedButton()
 {
     for(let button of languageButtons)
@@ -20,7 +22,7 @@ function ResetSelectedButton()
     }
 }
 
-
+//Styles the language buttons from gtranslate (external translation services based on google translate)
 function StyleButtons() {
     let languageButtons2 = document.querySelectorAll('[class*="glink nturl notranslate"]');
     document.getElementById("demo").innerText = languageButtons2.length;
--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,14 @@
-const stack = document.querySelector('.stack');
-const navLinks = document.querySelector('.navLinks');
+const stack = document.querySelector('.stack'); //Div with the stack
+const navLinks = document.querySelector('.navLinks'); //Div with all the navigation links
 let menuOpen = false;
 
+//Adds an interaction to the stack block that opens and closes the navigation menu
 stack.addEventListener('click', () => {
     if (!menuOpen) {
         navLinks.style.display = "block";
         menuOpen = true;
     }else if (menuOpen) {
-        navLinks.style.display = "none";
+        navLinks.style.display = "";
         menuOpen = false;
     }
 });
