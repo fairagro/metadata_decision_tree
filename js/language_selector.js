a73b74f0503e9fbd8e99ab9447b0a235839a3477
EinzJustus <Justus.H.Schneider@gmx.de>
2026-06-15 15:56:09 +0200

    Added custom translated english decision tree, changed language selection system

--- js/language_selector.js
+++ js/language_selector.js
@@ -9,25 +9,63 @@ function SetLanguage(language) {
     //document.getElementById("demo").innerText = currentLanguage;
 
     //Set current data path to the selected language
-    currentDataPath = `./data_${language}.xml`; 
-    LoadDoc(prevID.at(-1))
+    currentDataPath = `./data_${language}.xml`;
+    SetNavigatorLabels();
+}
+
+//Sets the current language and updates the Decision Tree UI
+function SetLanguageAndRefreshDT(language) {
+    SetLanguage(language);
+    ApplyLanguageToUI();
+    LoadDoc(prevID.at(-1));
 }
 
 //Sets all language buttons to their unselected design
-function ResetSelectedButton()
-{
-    for(let button of languageButtons)
-    {
+function ResetSelectedButton() {
+    for (let button of languageButtons) {
         button.className = "language";
     }
 }
 
-//Styles the language buttons from gtranslate (external translation services based on google translate)
-function StyleButtons() {
-    let languageButtons2 = document.querySelectorAll('[class*="glink nturl notranslate"]');
-    document.getElementById("demo").innerText = languageButtons2.length;
-    for(let button of languageButtons2)
-    {
-        button.className = "language";
+//Sets contents of all navigator elements to current language
+function SetNavigatorLabels() {
+    var xmlRequest = new XMLHttpRequest();
+    xmlRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var xmlDoc = this.responseXML; //Get XML document
+
+            var ui = xmlDoc.getElementsByTagName("UI");
+            var nav = ui[0].getElementsByTagName("NAVIGATOR"); //List of all "UI" entries in the XML document
+
+            document.getElementById("nav_decisiontree").innerText = nav[0].getElementsByTagName("DECISIONTREE")[0].childNodes[0].nodeValue;
+            document.getElementById("nav_glossar").innerText = nav[0].getElementsByTagName("GLOSSARY")[0].childNodes[0].nodeValue;
+            document.getElementById("nav_metadataguide").innerText = nav[0].getElementsByTagName("METADATAGUIDE")[0].childNodes[0].nodeValue;
+            document.getElementById("nav_contact").innerText = nav[0].getElementsByTagName("CONTACT")[0].childNodes[0].nodeValue;
+            document.getElementById("nav_bibliography").innerText = nav[0].getElementsByTagName("BIBLIOGRAPHY")[0].childNodes[0].nodeValue;
+        }
+    };
+    xmlRequest.open("GET", currentDataPath, true);
+    xmlRequest.send();
+}
+
+// Load data from XML and aplly to Decision Tree UI
+function ApplyLanguageToUI() {
+    var xmlRequest = new XMLHttpRequest();
+    xmlRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var xmlDoc = this.responseXML; //Get XML document
+            var ui = xmlDoc.getElementsByTagName("UI"); //List of all "UI" entries in the XML document
+            SetUILabels(ui);
+        }
+    };
+    xmlRequest.open("GET", currentDataPath, true);
+    xmlRequest.send();
+}
+
+//Set Decision Tree UI contents to current language
+function SetUILabels(ui) {
+    var textElement = ui[0].getElementsByTagName("TEXTELEMENT");
+    if (document.getElementById("back") != null) {
+        document.getElementById("back").innerText = textElement[0].getElementsByTagName("BACKBUTTON")[0].childNodes[0].nodeValue;
     }
 }
